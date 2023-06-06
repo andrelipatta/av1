e6164515739007331e1219517a3ae6627425fc41
andrelipatta <135712979+andrelipatta@users.noreply.github.com>
2023-06-06 11:08:03 -0300

    Update avaliação_1.py

--- avaliação_1.py
+++ avaliação_1.py
@@ -1,1 +1,27 @@
+import PySimpleGUI as sg
 
+sg.theme('Dark Amber') 
+
+layout = [ 
+  
+              [sg.Text('Índice de massa corpórea')],
+              [sg.Text('Massa: '),sg.Input(key='-MASS-' , size=(20,1))],
+              [sg.Text('Altura: '),sg.Input(key='-HEIGHT-' , size=(20,1))],
+              [sg.Push(),sg.Button('Calcular'),sg.Push()]
+          ]
+    
+    
+window = sg.window('IMC', layout=layout, font='Monaco 18')
+
+
+while True:
+    event, value = window.read() 
+    print(event, value)
+    massa = float(value['-MASS-'])
+    altura = float(value['-HEIGHT-'])
+    imc = massa/(altura**2)
+    sg.Popup(f'SEU IMC É {imc:.2f}' , font='26'
+    if event == sg.WIN_CLOSED or event == 'Exit' :
+       break
+
+window.close()
